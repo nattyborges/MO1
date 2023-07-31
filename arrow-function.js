e6d117d70e334c36c563228b87411b488709dc4a
Natália Borges <nattyborgesc@gmail.com>
2023-07-31 10:26:41 -0300

    aula

---
+++ arrow-function.js
@@ -0,0 +1,18 @@
+//DECLARAÇÃO TRADICIONAL
+
+function soma(x, y) {
+    //codigo
+    const resultado = x + y
+    return resultado
+}
+
+
+//DECLARAÇÃO ARROW FUNCTION
+const soma = (x, y) => {
+    //codigo
+    const resultado = x + y
+    return resultado
+}
+
+console.log(soma(5, 2))
+//...
