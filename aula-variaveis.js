f39629822a6511894a4b4761d2d314b7bf19dba6
Natália Borges <nattyborgesc@gmail.com>
2023-07-31 10:37:49 -0300

    aula

---
+++ aula-variaveis.js
@@ -0,0 +1,11 @@
+const cidadeNatal = "São Paulo"
+
+console.log(cidadeNatal)
+
+let populacaoLondrina = 588.125
+
+console.log(populacaoLondrina)
+
+populacaoLondrina = 654.321
+
+console.log(populacaoLondrina)
